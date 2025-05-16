4544d80a4270b92d9d0f25319da4aa346e6952bf
everson <esteveseverson@gmail.com>
2025-05-16 20:28:00 -0300

    feat: chaging landing page by churches table

--- src/erp_ipba/views.py
+++ src/erp_ipba/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from church.models import Church
 
 def landing_page(request: HttpRequest) -> HttpResponse:
-    return render(request, 'landing_page.html')
+    churches = Church.objects.all()
+    context = {'churches': churches}
+
+    return render(request, 'landing_page.html', context)
